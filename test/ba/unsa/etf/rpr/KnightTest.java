425eac45c34e52a3657777670e7a07c4e31df9d1
nadjasokolovic <nsokolovic1@etf.unsa.ba>
2018-11-10 15:25:10 +0100

    Dodani teestovi u Knight

--- test/ba/unsa/etf/rpr/KnightTest.java
+++ test/ba/unsa/etf/rpr/KnightTest.java
@@ -14,4 +14,43 @@ class KnightTest {
         );
 
     }
+
+    @Test
+    void illegalPosition() {
+        Knight k = new Knight("g1", ChessPiece.Color.WHITE);
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> k.move("3b")
+        );
+    }
+
+    @Test
+    void moveUpOrDown() {
+        Knight k = new Knight("d4", ChessPiece.Color.WHITE);
+        assertAll(
+                () -> assertThrows(
+                    IllegalChessMoveException.class,
+                        () -> k.move("d5")
+                ),
+                () -> assertThrows(
+                    IllegalChessMoveException.class,
+                        () -> k.move("d3")
+                )
+        );
+    }
+
+    @Test
+    void someLegalMoves() {
+        Knight k = new Knight("d4", ChessPiece.Color.WHITE);
+        assertDoesNotThrow(
+                ()-> {
+                    k.move("e6");
+                    k.move("g5");
+                    k.move("f3");
+                    k.move("e5");
+                }
+        );
+
+
+    }
 }
